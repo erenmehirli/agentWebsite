74b0bb17a8e525d9587ea00e18b66432c944d8d2
erenmehirli <hemi.eren@gmail.com>
2024-09-21 16:11:02 +0300

    .

---
+++ src/routers/RoutePath.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+
+const RoutePath = () => {
+    return (
+        <Routes>
+          <Route/>
+          
+        </Routes>
+    );
+}
+
+export default RoutePath;
